8905faa6e298d1736008948ea398fc6e2198b0c7
lucas7796 <lucas.pineda@hotmail.com>
2020-07-26 10:27:31 -0300

    desafio 02

--- tipos/tipos.js
+++ tipos/tipos.js
@@ -129,3 +129,16 @@ var contato1 = {
 };
 console.log(contato1.tel2);
 var podeSerNulo = null;
+var contaBancaria = {
+    saldo: 3456,
+    depositar: function (valor) {
+        this.saldo += valor;
+    }
+};
+var correntista = {
+    nome: 'Ana Silva',
+    contaBancaria: contaBancaria,
+    contatos: ['34567890', '98765432']
+};
+correntista.contaBancaria.depositar(3000);
+console.log(correntista);
--- tipos/tipos.ts
+++ tipos/tipos.ts
@@ -176,3 +176,27 @@ const contato1: Contato = {
 console.log(contato1.tel2)
 
 let podeSerNulo: null = null
+
+
+// Desafio 02: rtanformar codigo JS em TS 
+
+type ContaBancaria = {
+  saldo: number,
+  depositar: (valor: number) => void
+}
+
+let contaBancaria: ContaBancaria = {
+  saldo: 3456,
+  depositar(valor) {
+      this.saldo += valor
+  }
+}
+
+let correntista: { nome: string, contaBancaria: ContaBancaria, contatos: string[] } = {
+  nome: 'Ana Silva',
+  contaBancaria: contaBancaria,
+  contatos: ['34567890', '98765432']
+}
+
+correntista.contaBancaria.depositar(3000)
+console.log(correntista)
