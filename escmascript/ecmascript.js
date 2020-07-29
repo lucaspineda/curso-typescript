82a37def48f03635817e1ddd8722d1496a8d83c2
lucas7796 <lucas.pineda@hotmail.com>
2020-07-29 00:51:11 -0300

    let const

---
+++ escmascript/ecmascript.js
@@ -0,0 +1,12 @@
+"use strict";
+// let & const
+let estaFrio = true;
+if (estaFrio) {
+    let acao = 'colocar casado';
+    console.log(acao);
+}
+// console.log(acao) - nao pode pois let tem escopo de bloco, funcao e global
+// consts nao podem ser redeclaradas
+// const tbm tem escopo de bloco, funcao e global
+// var nao apenas nao tem escopo de bloco
+//# sourceMappingURL=ecmascript.js.map
---
+++ escmascript/ecmascript.ts
@@ -0,0 +1,14 @@
+// let & const
+
+let estaFrio = true
+if(estaFrio) {
+  let acao = 'colocar casado'
+  console.log(acao)
+}
+// console.log(acao) - nao pode pois let tem escopo de bloco, funcao e global
+
+// consts nao podem ser redeclaradas
+
+// const tbm tem escopo de bloco, funcao e global
+
+// var nao apenas nao tem escopo de bloco
