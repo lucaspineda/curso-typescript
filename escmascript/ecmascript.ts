e7331b4e8cdf0ef4081cfb065619ff0fdc7e2dae
lucas7796 <lucas.pineda@hotmail.com>
2020-08-21 01:46:40 -0300

    exercicios ts

--- escmascript/ecmascript.ts
+++ escmascript/ecmascript.ts
@@ -91,3 +91,63 @@ const item = {
 
 const { nome: nm } = item
 console.log(nm)
+
+// Exercicios Ecmascript //
+
+// Exercicio 1
+// var dobro = function(valor) {
+//   return valor * 2
+// }
+
+// resposta: 
+
+const dobro = (valor: number) : number => valor * 2
+
+console.log(dobro(10))
+
+// Exercicio 2
+// var dizerOla = function (nome) {
+//   if (nome === undefined) { nome = "Pessoa" }
+//   console.log("Ola, " + nome)
+// }
+
+const dizerOla = (nome: string = 'Pessoa') => {
+  console.log("Ola, " + nome)
+}
+
+dizerOla()
+dizerOla("Anna")
+
+// Exercicio 3
+// var nums = [-3, 33, 38, 5]
+
+// resposta
+var nums: number[] = [-3, 33, 38, 5]
+
+console.log(...nums)
+
+// Exercicio 4
+var array = [55, 20]
+
+console.log(array)
+
+// Exercicio 5
+// var notas = [8.5, 6.3, 9.4]
+// var notas1 = notas[0]
+// var notas2 = notas[1]
+// var notas3 = notas[2] 
+
+// resposta
+const notas: number[] = [8.5, 6.3, 9.4]
+const [nota1, nota2, nota3] = notas
+console.log(nota1, nota2, nota3)
+
+// Exercicio 6
+// const cientista = {primeiroNome: "Will", experiencia: 12}
+// var primeiroNome = cientista.primeiroNome
+// var experiencia = cientista.experiencia
+
+// Exercicio 6 resposta
+const cientista = {primeiroNome: "Will", experiencia: 12}
+const {primeiroNome, experiencia} = cientista
+console.log(primeiroNome, experiencia)
