ed47e0482696934730fa87a72026eaaaece7b0c8
lucas7796 <lucas.pineda@hotmail.com>
2020-08-23 01:06:34 -0300

    callback vs promises

--- escmascript/ecmascript.js
+++ escmascript/ecmascript.js
@@ -61,4 +61,69 @@ const item = {
 };
 const { nome: nm } = item;
 console.log(nm);
+// Exercicios Ecmascript //
+// Exercicio 1
+// var dobro = function(valor) {
+//   return valor * 2
+// }
+// resposta: 
+const dobro = (valor) => valor * 2;
+console.log(dobro(10));
+// Exercicio 2
+// var dizerOla = function (nome) {
+//   if (nome === undefined) { nome = "Pessoa" }
+//   console.log("Ola, " + nome)
+// }
+const dizerOla = (nome = 'Pessoa') => {
+    console.log("Ola, " + nome);
+};
+dizerOla();
+dizerOla("Anna");
+// Exercicio 3
+// var nums = [-3, 33, 38, 5]
+// resposta
+var nums = [-3, 33, 38, 5];
+console.log(...nums);
+// Exercicio 4
+var array = [55, 20];
+console.log(array);
+// Exercicio 5
+// var notas = [8.5, 6.3, 9.4]
+// var notas1 = notas[0]
+// var notas2 = notas[1]
+// var notas3 = notas[2] 
+// resposta
+const notas = [8.5, 6.3, 9.4];
+const [nota1, nota2, nota3] = notas;
+console.log(nota1, nota2, nota3);
+// Exercicio 6
+// const cientista = {primeiroNome: "Will", experiencia: 12}
+// var primeiroNome = cientista.primeiroNome
+// var experiencia = cientista.experiencia
+// Exercicio 6 resposta
+const cientista = { primeiroNome: "Will", experiencia: 12 };
+const { primeiroNome, experiencia } = cientista;
+console.log(primeiroNome, experiencia);
+// callback
+function esperar3s(callback) {
+    setTimeout(() => {
+        callback('3 segundos depois;;;');
+    }, 3000);
+}
+esperar3s(function (resultado) {
+    console.log(resultado);
+});
+// const resultado = esperar3s()
+// console.log(resultado) //retorna undefined
+// promise
+function esperar3spromise() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve('3 segundos depois;;;');
+        }, 3000);
+    });
+}
+esperar3spromise()
+    .then(dado => console.log(dado));
+// com promises as chamadas sao melhores encadeadas, pode usar multiplos then
 //# sourceMappingURL=ecmascript.js.map
--- escmascript/ecmascript.ts
+++ escmascript/ecmascript.ts
@@ -151,3 +151,35 @@ console.log(nota1, nota2, nota3)
 const cientista = {primeiroNome: "Will", experiencia: 12}
 const {primeiroNome, experiencia} = cientista
 console.log(primeiroNome, experiencia)
+
+// callback
+
+function esperar3s(callback) {
+  setTimeout(() => {
+    callback('3 segundos depois;;;')
+  }, 3000);
+}
+
+esperar3s(function(resultado) {
+  console.log(resultado)
+})
+
+// const resultado = esperar3s()
+// console.log(resultado) //retorna undefined
+
+
+// promise
+
+function esperar3spromise() {
+  return new Promise((resolve: any) => {
+    setTimeout(() => {
+      resolve('3 segundos depois;;;')
+    }, 3000);
+  })
+}
+
+esperar3spromise()
+  .then(dado => console.log(dado))
+  // com promises as chamadas sao melhores encadeadas, pode usar multiplos then
+
+
