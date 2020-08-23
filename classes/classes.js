b419aad60e278764b768125561a6ab3766f68b42
lucas7796 <lucas.pineda@hotmail.com>
2020-08-23 01:30:36 -0300

    classe mais curta

--- classes/classes.js
+++ classes/classes.js
@@ -10,4 +10,15 @@ const aniversario = new Data(3, 11, 1991);
 aniversario.dia = 4;
 console.log(aniversario.dia);
 const casamento = new Data; //pode-se omitir os parenteses devido ao valor padrao
+class DataEsperta {
+    constructor(dia = 1, mes = 1, ano = 1970) {
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+    }
+} // exatamento a mesma classe porem declarado de forma mais curta
+const aniversarioEsperto = new DataEsperta(3, 11, 1991);
+aniversario.dia = 4;
+console.log(aniversario.dia);
+const casamentoEsperto = new DataEsperta; //pode-se omitir os parenteses devido ao valor padrao
 //# sourceMappingURL=classes.js.map
--- classes/classes.ts
+++ classes/classes.ts
@@ -16,3 +16,14 @@ aniversario.dia = 4
 console.log(aniversario.dia)
 
 const casamento = new Data //pode-se omitir os parenteses devido ao valor padrao
+
+class DataEsperta {
+  constructor(public dia: number = 1, public mes: number = 1, public ano: number = 1970) {
+  }
+} // exatamento a mesma classe porem declarado de forma mais curta
+
+const aniversarioEsperto = new DataEsperta(3, 11, 1991)
+aniversario.dia = 4
+console.log(aniversario.dia)
+
+const casamentoEsperto = new DataEsperta //pode-se omitir os parenteses devido ao valor padrao
