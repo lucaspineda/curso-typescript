ca14f5ca243dc210b777e3db88e6f899afceb879
lucas7796 <lucas.pineda@hotmail.com>
2020-08-30 17:35:58 -0300

    modificadores de acesso publicos e privados

--- classes/classes.ts
+++ classes/classes.ts
@@ -46,3 +46,37 @@ class Produto {
 const produto1 = new Produto('Caneta bic', 4.20, 0.9)
 console.log(produto1)
 console.log(produto1.resumo())
+
+class Carro {
+  private velocidadeAtual: number = 0
+
+  constructor(public marca: string, public modelo: string, private velocidadeMaxima: number = 200){
+     
+  }
+
+  private alterarVelocidade(delta: number): number {
+    const novaVelocidade = this.velocidadeAtual + delta;
+    const velocidadeValida = novaVelocidade >= 0 && novaVelocidade <= this.velocidadeMaxima
+
+    if(velocidadeValida) {
+      this.velocidadeAtual = novaVelocidade
+    } else {
+      this.velocidadeAtual = delta > 0 ? this.velocidadeMaxima : 0
+    }
+    return this.velocidadeAtual;
+  }
+
+  public acelerar(): number {
+    return this.alterarVelocidade(5)
+  }
+
+  public frear(): number {
+    return this.alterarVelocidade(-5)
+  }
+
+}
+
+const carro1 = new Carro('Ford', 'ka', 100)
+console.log(carro1.acelerar());
+// console.log(carro1.alterarVelocidade()); //este metodo nao pode ser acessada pois é privada.
+
