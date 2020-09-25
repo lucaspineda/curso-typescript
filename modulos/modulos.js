cde772a6b4b5a1741b6122dbdee19e8590166e97
lucas7796 <lucas.pineda@hotmail.com>
2020-09-25 00:58:39 -0300

    create modules

---
+++ modulos/modulos.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const retangulo_1 = require("./retangulo");
+const circunferencia_1 = require("./circunferencia");
+// import retangulo from './retangulo'
+// import { areaCircunferencia as circ } from './circunferencia'
+console.log(retangulo_1.areaRetangulo(7, 8));
+console.log(circunferencia_1.areaCircunferencia(8));
+// console.log('Módulo carregado...')
+// console.log(retangulo(7, 8))
+// console.log(circ(2))
+// const { digaOi } = require('./novo')
+// console.log(digaOi('Ana'))
+//# sourceMappingURL=modulos.js.map
