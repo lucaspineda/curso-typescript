c64255168dcfafcfa3c1476ef8fe4b129e57c150
lucas7796 <lucas.pineda@hotmail.com>
2020-09-26 23:47:11 -0300

    another options to export modules

--- modulos/modulos.js
+++ modulos/modulos.js
@@ -1,10 +1,13 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const retangulo_1 = require("./retangulo");
+const retangulo_1 = __importDefault(require("./retangulo"));
 const circunferencia_1 = require("./circunferencia");
 // import retangulo from './retangulo'
 // import { areaCircunferencia as circ } from './circunferencia'
-console.log(retangulo_1.areaRetangulo(7, 8));
+console.log(retangulo_1.default(7, 8));
 console.log(circunferencia_1.areaCircunferencia(8));
 // console.log('Módulo carregado...')
 // console.log(retangulo(7, 8))
--- modulos/modulos.ts
+++ modulos/modulos.ts
@@ -1,10 +1,10 @@
-import { areaRetangulo } from './retangulo'
-import { areaCircunferencia } from './circunferencia'
+import areaRetangulo from './retangulo'
+import { areaCircunferencia as circ} from './circunferencia'
 // import retangulo from './retangulo'
 // import { areaCircunferencia as circ } from './circunferencia'
 
 console.log(areaRetangulo(7,8))
-console.log(areaCircunferencia(8))
+console.log(circ(8))
 // console.log('Módulo carregado...')
 // console.log(retangulo(7, 8))
 // console.log(circ(2))
