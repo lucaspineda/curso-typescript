151b6fd649467245824821e0c22bbc6662cd3a71
lucas7796 <lucas.pineda@hotmail.com>
2020-10-14 01:08:53 -0300

    apply decorator in a attribute

--- decorators/decorators.ts
+++ decorators/decorators.ts
@@ -86,68 +86,68 @@ function perfilAdmin<T extends Construtor>(construtor: T) {
 }
 
 
-// class ContaCorrente {
-//   @naoNegativo
-//   private saldo: number
+class ContaCorrente {
+  @naoNegativo
+  private saldo: number
 
-//   constructor(saldo: number) {
-//       this.saldo = saldo
-//   }
+  constructor(saldo: number) {
+      this.saldo = saldo
+  }
 
-//   @congelar
-//   sacar(@paramInfo valor: number) {
-//       if(valor <= this.saldo) {
-//           this.saldo -= valor
-//           return true
-//       } else {
-//           return false
-//       }
-//   }
+  @congelar
+  sacar(@paramInfo valor: number) {
+    if(valor <= this.saldo) {
+      this.saldo -= valor
+      return true
+    } else {
+      return false
+    }
+  }
 
-//   @congelar
-//   getSaldo() {
-//       return this.saldo
-//   }
+  @congelar
+  getSaldo() {
+      return this.saldo
+  }
+}
+
+const cc = new ContaCorrente(10248.57)
+cc.sacar(5000)
+cc.sacar(5248.57)
+cc.sacar(0.1)
+console.log(cc.getSaldo())
+
+// cc.getSaldo = function() {
+//     return this['saldo'] + 7000
 // }
+console.log(cc.getSaldo())
 
-// const cc = new ContaCorrente(10248.57)
-// cc.sacar(5000)
-// cc.sacar(5248.57)
-// cc.sacar(0.1)
-// console.log(cc.getSaldo())
+// Object.freeze()
+function congelar(alvo: any, nomeMetodo: string,
+  descritor: PropertyDescriptor) {
+  console.log(alvo)
+  console.log(nomeMetodo)
+  descritor.writable = false
+}
 
-// // cc.getSaldo = function() {
-// //     return this['saldo'] + 7000
-// // }
-// console.log(cc.getSaldo())
+function naoNegativo(alvo: any, nomePropriedade: string) {
+  delete alvo[nomePropriedade]
+  Object.defineProperty(alvo, nomePropriedade, {
+    get: function(): any {
+      return alvo["_" + nomePropriedade]
+    },
+    set: function(valor: any): void {
+      if(valor < 0) {
+        throw new Error('Saldo Inválido')
+      } else {
+        alvo["_" + nomePropriedade] = valor
+      }
+    }
+  })
+}
 
-// // Object.freeze()
-// function congelar(alvo: any, nomeMetodo: string,
-//   descritor: PropertyDescriptor) {
-//   console.log(alvo)
-//   console.log(nomeMetodo)
-//   descritor.writable = false
-// }
-
-// function naoNegativo(alvo: any, nomePropriedade: string) {
-//   delete alvo[nomePropriedade]
-//   Object.defineProperty(alvo, nomePropriedade, {
-//       get: function(): any {
-//           return alvo["_" + nomePropriedade]
-//       },
-//       set: function(valor: any): void {
-//           if(valor < 0) {
-//               throw new Error('Saldo Inválido')
-//           } else {
-//               alvo["_" + nomePropriedade] = valor
-//           }
-//       }
-//   })
-// }
-
-// function paramInfo(alvo: any, nomeMetodo: string,
-//   indiceParam: number) {
-//   console.log(`Alvo: ${alvo}`)
-//   console.log(`Método: ${nomeMetodo}`)
-//   console.log(`Índice Param: ${indiceParam}`)
-// }
+function paramInfo(alvo: any, nomeMetodo: string,
+  indiceParam: number) {
+  console.log(`Alvo: ${alvo}`)
+  console.log(`Método: ${nomeMetodo}`)
+  console.log(`Índice Param: ${indiceParam}`)
+}
