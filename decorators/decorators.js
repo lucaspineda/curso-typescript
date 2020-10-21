b6fd01b56803d74a935000e2e7b6c713921cc2f7
lucaspineda <lucas.pineda@hotmail.com>
2020-10-20 22:13:02 -0300

    parameter decorator

--- decorators/decorators.js
+++ decorators/decorators.js
@@ -5,6 +5,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 function logarClasse(construtor) {
     console.log(construtor);
 }
@@ -77,61 +80,67 @@ function perfilAdmin(construtor) {
         }
     };
 }
-// class ContaCorrente {
-//   @naoNegativo
-//   private saldo: number
-//   constructor(saldo: number) {
-//       this.saldo = saldo
-//   }
-//   @congelar
-//   sacar(@paramInfo valor: number) {
-//       if(valor <= this.saldo) {
-//           this.saldo -= valor
-//           return true
-//       } else {
-//           return false
-//       }
-//   }
-//   @congelar
-//   getSaldo() {
-//       return this.saldo
-//   }
+class ContaCorrente {
+    constructor(saldo) {
+        this.saldo = saldo;
+    }
+    sacar(valor) {
+        if (valor <= this.saldo) {
+            this.saldo -= valor;
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    getSaldo() {
+        return this.saldo;
+    }
+}
+__decorate([
+    naoNegativo
+], ContaCorrente.prototype, "saldo", void 0);
+__decorate([
+    congelar,
+    __param(0, paramInfo)
+], ContaCorrente.prototype, "sacar", null);
+__decorate([
+    congelar
+], ContaCorrente.prototype, "getSaldo", null);
+const cc = new ContaCorrente(10248.57);
+cc.sacar(5000);
+cc.sacar(5248.57);
+cc.sacar(0.1);
+console.log(cc.getSaldo());
+// cc.getSaldo = function() {
+//     return this['saldo'] + 7000
 // }
-// const cc = new ContaCorrente(10248.57)
-// cc.sacar(5000)
-// cc.sacar(5248.57)
-// cc.sacar(0.1)
-// console.log(cc.getSaldo())
-// // cc.getSaldo = function() {
-// //     return this['saldo'] + 7000
-// // }
-// console.log(cc.getSaldo())
-// // Object.freeze()
-// function congelar(alvo: any, nomeMetodo: string,
-//   descritor: PropertyDescriptor) {
-//   console.log(alvo)
-//   console.log(nomeMetodo)
-//   descritor.writable = false
-// }
-// function naoNegativo(alvo: any, nomePropriedade: string) {
-//   delete alvo[nomePropriedade]
-//   Object.defineProperty(alvo, nomePropriedade, {
-//       get: function(): any {
-//           return alvo["_" + nomePropriedade]
-//       },
-//       set: function(valor: any): void {
-//           if(valor < 0) {
-//               throw new Error('Saldo Inválido')
-//           } else {
-//               alvo["_" + nomePropriedade] = valor
-//           }
-//       }
-//   })
-// }
-// function paramInfo(alvo: any, nomeMetodo: string,
-//   indiceParam: number) {
-//   console.log(`Alvo: ${alvo}`)
-//   console.log(`Método: ${nomeMetodo}`)
-//   console.log(`Índice Param: ${indiceParam}`)
-// }
+console.log(cc.getSaldo());
+// Object.freeze()
+function congelar(alvo, nomeMetodo, descritor) {
+    console.log(alvo);
+    console.log(nomeMetodo);
+    descritor.writable = false;
+}
+function naoNegativo(alvo, nomePropriedade) {
+    delete alvo[nomePropriedade];
+    Object.defineProperty(alvo, nomePropriedade, {
+        get: function () {
+            return alvo["_" + nomePropriedade];
+        },
+        set: function (valor) {
+            if (valor < 0) {
+                throw new Error('Saldo Inválido');
+            }
+            else {
+                alvo["_" + nomePropriedade] = valor;
+            }
+        }
+    });
+}
+function paramInfo(alvo, nomeMetodo, indiceParam) {
+    console.log(`Alvo: ${alvo}`);
+    console.log(`Método: ${nomeMetodo}`);
+    console.log(`Índice Param: ${indiceParam}`);
+}
 //# sourceMappingURL=decorators.js.map
